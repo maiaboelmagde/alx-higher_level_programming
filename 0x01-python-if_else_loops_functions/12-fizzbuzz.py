1f5816481e13f45bcb7fda311ce9eccbf4ec1ff3
mai <maishalan00000@gmail.com>
2024-04-04 18:10:23 +0200

    uploading the 12th task solution

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    indx=0
+    indx = 0
     while (indx < 100):
         flag = 1
         indx += 1
         if (indx % 3 == 0):
-            print('Fizz',end='')
+            print('Fizz', end='')
             flag = 0
         if (indx % 5 == 0):
-            print('Buzz',end='')
+            print('Buzz', end='')
             flag = 0
         if flag:
             print(indx, end='')
