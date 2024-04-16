55c3f06b1878036f6c6b7dc06c2bd9c194583008
mai <maishalan00000@gmail.com>
2024-04-16 02:14:24 +0200

    fixing pyCodeStyle

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,5 +2,8 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-if(__name__ == "__main__"):
-    print('{} + {} = {}\n{} - {} = {}\n{} * {} = {} \n{} / {} = {}'.format(a, b, add(a, b), a, b, sub(a, b),a, b, mul(a, b), a, b, div(a, b)))
+if (__name__ == "__main__"):
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{} - {} = {}'.format(a, b, sub(a, b)))
+    print('{} * {} = {}'.format(a, b, mul(a, b)))
+    print('{} / {} = {}'.format(a, b, div(a, b)))
