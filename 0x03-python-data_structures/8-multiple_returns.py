c2563bf4557a196ba2115c1c52fd68dfb5e34b74
mai <maishalan00000@gmail.com>
2024-04-22 18:42:29 +0200

    the 8th task solution

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''
+function that returns a tuple with:
+the length of a string
+and its first character
+'''
+
+
+def multiple_returns(sentence):
+    return (len(sentence), sentence[0])
