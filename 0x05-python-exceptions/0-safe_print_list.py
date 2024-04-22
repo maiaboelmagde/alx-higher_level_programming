5664b170485cf8d82e61717de7107d9ed3716fa1
mai <maishalan00000@gmail.com>
2024-04-22 23:22:30 +0200

    fixing The 0th task solution

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,12 +4,11 @@
 
 def safe_print_list(my_list=[], x=0):
     idx = 0
-    try:
-        while x > idx:
+    while x > idx:
+        try:
             print(my_list[idx], end='')
             idx += 1
-        print()
-        return idx
-    except Exception:
-        print()
-        return idx
+        except Exception:
+            break
+    print()
+    return idx
