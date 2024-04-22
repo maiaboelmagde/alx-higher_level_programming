6671a954cf649750f9504c034d93a2911206aa6c
mai <maishalan00000@gmail.com>
2024-04-22 23:25:56 +0200

    fixing The 0th task solution

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
+''' function that prints x elements of a list.'''
+
 
 def safe_print_list(my_list=[], x=0):
-    sum_total = 0
-    for i in range(x):
+    idx = 0
+    while x > idx:
         try:
-            print(f"{my_list[i]}", end="")
-            sum_total += 1
-        except IndexError:
+            print(f"{my_list[idx]}", end='')
+            idx += 1
+        except Exception:
             break
     print()
-    return(sum_total)
+    return idx
