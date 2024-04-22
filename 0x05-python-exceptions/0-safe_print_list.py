e21b8ce66624a5d2478230de0060422b0e5f5fca
mai <maishalan00000@gmail.com>
2024-04-22 23:24:14 +0200

    fixing The 0th task solution

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     idx = 0
     while x > idx:
         try:
-            print(my_list[idx], end='')
+            print(f"{my_list[idx]}", end='')
             idx += 1
         except Exception:
             break
