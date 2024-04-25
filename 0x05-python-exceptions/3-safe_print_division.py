3b74fe5efb2bd5caa38746dd3e8f9142796d4506
mai <maishalan00000@gmail.com>
2024-04-25 22:58:20 +0200

    The 4th task solution

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,10 @@ function that divides 2 integers and prints the result.
 
 def safe_print_division(a, b):
     try:
-        return a/b
+        returnValue = a/b
+        return returnValue
     except ZeroDivisionError:
-        return None
+        returnValue = None
+        return returnValue
+    finally:
+        print("Inside result: {}".format(returnValue))
