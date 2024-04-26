9fac896a731651b5036c49309dec80e0a3dbe168
mai <maishalan00000@gmail.com>
2024-04-26 22:09:16 +0300

    The 4th task solution

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''function that divides element by element 2 lists.'''
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    newList = []
+    for i in range(list_length):
+        try:
+            newList.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print('wrong type')
+            newList.append(0)
+        except ZeroDivisionError:
+            print('division by 0')
+            newList.append(0)
+        except Exception:
+            print('out of range')
+            newList.append(0)
+    return newList
